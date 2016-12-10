183006ba72adc5fb3f7ee1b55ec9f8bc37d77140
Ashley Ross <ashley.e.ross@gmail.com>
2016-12-10 15:39:41 -0800

    adding form functions

---
+++ app.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var userType = [];
+var sortedUsers = [];
+var submitForm = document.getElementById('submit');
+submitForm.addEventListener('click', storeData);
+// var icon1C = document.getElementById('userPreference');
+// icon1C.addEventListener('click', match);
+
+function storeData() {
+
+}
+
+function match() {
+
+}
